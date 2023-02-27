30de1dfa9e253960c7ba4f7222c803221dfa31c4
Patrick Amadeus Irawan <patrickai0309@gmail.com>
2023-02-27 18:24:55 +0700

    feat: adding entities

---
+++ entities/responses/group_response.go
@@ -0,0 +1,40 @@
+package responses
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type UserGroupResponse struct {
+	GroupID      uuid.UUID   `json:"group_id"`
+	MemberID     []uuid.UUID `json:"member_id"`
+	StartDate    time.Time   `json:"start_date"`
+	EndDate      time.Time   `json:"end_date"`
+	Type         string      `json:"type"`
+	TotalUnpaid  float64     `json:"total_unpaid"`
+	TotalExpense float64     `json:"total_expense"`
+}
+
+type GroupDetailResponse struct {
+	GroupID    uuid.UUID      `json:"group_id"`
+	MemberID   []uuid.UUID    `json:"member_id"`
+	StartDate  time.Time      `json:"start_date"`
+	EndDate    time.Time      `json:"end_date"`
+	ListMember []MemberDetail `json:"list_member"`
+}
+
+type GroupTransactionsResponse struct {
+	TransactionID uuid.UUID   `json:"group_id"`
+	Name          string      `json:"name"`
+	Description   string      `json:"description"`
+	Total         float64     `json:"total"`
+	BillOwner     uuid.UUID   `json:"bill_owner"`
+	ListMember    []uuid.UUID `json:"list_member"`
+}
+
+type MemberDetail struct {
+	ID          uuid.UUID `json:"member_id"`
+	Type        string    `json:"type"`
+	TotalUnpaid float64   `json:"total_unpaid"`
+}
