e93b82fa8f7d2e4251ca8503a71d08d63f6df20b
Patrick Amadeus Irawan <patrickai0309@gmail.com>
2023-03-01 16:00:49 +0700

    feat: adding []uuid types

--- entities/group.go
+++ entities/group.go
@@ -1,15 +1,16 @@
 package entities
 
 import (
+	"split-rex-backend/types"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 type Group struct {
-	GroupID   uuid.UUID   `gorm:"not null;unique"`
-	Name      string      `gorm:"not null"`
-	MemberID  []uuid.UUID `gorm:"not null;type:"`
-	StartDate time.Time   `gorm:"not null"`
-	EndDate   time.Time   `gorm:"not null"`
+	GroupID   uuid.UUID         `gorm:"not null;unique"`
+	Name      string            `gorm:"not null"`
+	MemberID  types.ArrayOfUUID `gorm:"not null"`
+	StartDate time.Time         `gorm:"not null"`
+	EndDate   time.Time         `gorm:"not null"`
 }
