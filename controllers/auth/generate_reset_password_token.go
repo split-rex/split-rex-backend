4d72c9aa24bc5fdfe7304f43a0ab8d7fa58825cf
graceclaudia1719 <grcxlr@gmail.com>
2023-04-26 11:47:36 +0700

    fix: comment + add response for testing purpose

--- controllers/auth/generate_reset_password_token.go
+++ controllers/auth/generate_reset_password_token.go
@@ -105,7 +105,8 @@ func (con *authController) GenerateResetPassTokenController(c echo.Context) erro
 	}
 	token := randStringBytesMaskImprSrcUnsafe(10)
 	// encryptedToken := gcm.Seal(nonce, nonce, []byte(token), nil)
-	encryptedToken := base64.StdEncoding.EncodeToString(gcm.Seal(nonce, nonce, []byte(token), nil))
+	seal := gcm.Seal(nonce, nonce, []byte(token), nil)
+	encryptedToken := base64.StdEncoding.EncodeToString(seal)
 
 	// generate random for code
 	code := randStringBytesMaskImprSrcUnsafe(6)
@@ -136,30 +137,30 @@ func (con *authController) GenerateResetPassTokenController(c echo.Context) erro
 		}
 	}
 	// send to email the code
-	emailSenderName := os.Getenv("EMAIL_SENDER_NAME")
-	emailSenderAddress := os.Getenv("EMAIL_SENDER_ADDRESS")
-	emailSenderPassword := os.Getenv("EMAIL_SENDER_PASSWORD")
-	sender := NewGmailSender(emailSenderName, emailSenderAddress, emailSenderPassword)
+	// emailSenderName := os.Getenv("EMAIL_SENDER_NAME")
+	// emailSenderAddress := os.Getenv("EMAIL_SENDER_ADDRESS")
+	// emailSenderPassword := os.Getenv("EMAIL_SENDER_PASSWORD")
+	// sender := NewGmailSender(emailSenderName, emailSenderAddress, emailSenderPassword)
 
-	subject := "Password Reset"
-	content := `
-	<h1>Request to Reset Your Password</h1>
-	<p>Someone requested a password reset at this email address for Splitrex Mobile.</p>
-	<p>To complete the reset password, enter the verification code below: </p>
-	<h2>` + code + `</h2>
-	<p>Code will expire in 2 minutes. If you did not request a password reset, you can safely ignore this email.</p>
-	<p>© 2023 Splitrex</p>
-	`
-	to := []string{user.Email}
-	// attachFiles := []string{"../README.md"}
+	// subject := "Password Reset"
+	// content := `
+	// <h1>Request to Reset Your Password</h1>
+	// <p>Someone requested a password reset at this email address for Splitrex Mobile.</p>
+	// <p>To complete the reset password, enter the verification code below: </p>
+	// <h2>` + code + `</h2>
+	// <p>Code will expire in 2 minutes. If you did not request a password reset, you can safely ignore this email.</p>
+	// <p>© 2023 Splitrex</p>
+	// `
+	// to := []string{user.Email}
 
-	err = sender.SendEmail(subject, content, to, nil, nil, nil)
-	if err != nil {
-		response.Message = types.ERROR_INTERNAL_SERVER
-		return c.JSON(http.StatusInternalServerError, response)
-	}
+	// err = sender.SendEmail(subject, content, to, nil, nil, nil)
+	// if err != nil {
+	// 	response.Message = types.ERROR_INTERNAL_SERVER
+	// 	return c.JSON(http.StatusInternalServerError, response)
+	// }
 
 	// return the passwordResetToken
+	response.Data.OnlyEncryptedToken = string(seal)
 	response.Data.EncryptedToken = encryptedToken
 	response.Data.Token = token
 
--- entities/responses/generate_reset_pass_token_response.go
+++ entities/responses/generate_reset_pass_token_response.go
@@ -1,6 +1,7 @@
 package responses
 
 type GenerateResetPassTokenResponse struct {
-	EncryptedToken string `json:"encrypted_token"`
-	Token          string `json:"token"`
+	EncryptedToken     string `json:"encrypted_token"`
+	OnlyEncryptedToken string `json:"only_encrypted_token"`
+	Token              string `json:"token"`
 }
